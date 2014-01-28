4aca7077ff94e4bd0db20f5983c26db613f1d606
David Cox-Espenlaub <DCoxespenlaub@resource.com>
2014-01-28 16:54:09 -0500

    #1 Stripped height adjustment code as it was causing issues with flowing layouts.

--- src/tabs.js
+++ src/tabs.js
@@ -11,18 +11,18 @@
     // on click of one of tabs
     var displayPage = function(e) {
       var $that = $(this),
-        o = e.data.o;
-      var $parent = $that.parent();
-      var current = $parent.attr("data-tab-current"),
-        ident = $that.attr("data-tab");
-      var currentSel = '[data-tab="' + current + '"]',
-        identSel = '[data-tab="' + ident + '"]';
-            var currHeight;
+        o = e.data.o,
+        $parent = $that.parent(),
+        current = $parent.attr("data-tab-current"),
+        ident = $that.attr("data-tab"),
+        currentSel = '[data-tab="' + current + '"]',
+        identSel = '[data-tab="' + ident + '"]',
+        currHeight;
 
       if(ident !== current){
 
-                // find the current height for the current content;
-                currHeight = $(o.cselector + currentSel).height();
+        // find the current height for the current content;
+        currHeight = $(o.cselector + currentSel).height();
 
         // hide current content
         $(o.selector + currentSel + ', '+ o.cselector + currentSel).removeClass(o.activeClass);
@@ -30,19 +30,8 @@
         // show new content
         $(o.selector + identSel + ', '+ o.cselector + identSel).addClass(o.activeClass);
 
-                // shim up the height of the new content so that the page doesn't shrink in height
-                // and then immediately grow again.  This is strictly for appearances.  A better user
-                // experience
-                $(o.cselector + identSel).height(currHeight);
-
         // update current data
         $parent.attr("data-tab-current",ident);
-
-                // clear our height  -- have to wait so that browser actually applies the height, otherwise javascript
-                // or perhaps browser optomization recognizes that height is getting erased and never sets the height.
-                window.setTimeout(function(){
-                    $(o.cselector + identSel).css('height','');
-                },2000);
 
         // call callback
         if($.isFunction(o.callback)) {
